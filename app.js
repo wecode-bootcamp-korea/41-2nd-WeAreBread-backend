c40dffeb77fe39a8c82a1ad2eb8bc333b1d2e743
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-30 16:56:15 +0900

    초기환경세팅

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const routes = require("./routes");
+
+const { globalErrorHandler } = require("./utils/error");
+
+const createApp = () => {
+  const app = express();
+
+  app.get("/ping", (req, res) => {
+    return res.status(200).json({ message: "pong" });
+  });
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(morgan("combined"));
+
+  app.use(routes);
+  app.use(globalErrorHandler);
+
+  return app;
+};
+
+module.exports = { createApp };
