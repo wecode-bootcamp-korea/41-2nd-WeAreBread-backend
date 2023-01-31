e8b43afcee8e5ba7e4ca80ec1df5bcc8d7a90853
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-01-31 17:19:29 +0900

    db migrations

---
+++ db/migrations/20230130034838_create_review_img_table.sql
@@ -0,0 +1,10 @@
+-- migrate:up
+CREATE TABLE review_images (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  img_url VARCHAR(1000) NOT NULL,
+  review_id INT NOT NULL,
+  CONSTRAINT review_images_review_id FOREIGN KEY (review_id) REFERENCES reviews(id)
+);
+
+-- migrate:down
+DROP TABLE review_images;
