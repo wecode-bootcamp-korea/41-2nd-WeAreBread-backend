866541dc725f2f1eccb2f4d74265a3d591d92dab
Kwang-Hwi Kim <kkh940227@gmail.com>
2023-02-09 11:12:18 +0900

    db 수정

--- db/migrations/20230130034838_create_review_images_table.sql
+++ db/migrations/20230130034838_create_review_images_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE review_images (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   img_url VARCHAR(1000) NOT NULL,
   review_id INT NOT NULL,
-  CONSTRAINT review_images_review_id FOREIGN KEY (review_id) REFERENCES reviews(id)
+  CONSTRAINT review_images_review_id FOREIGN KEY (review_id) REFERENCES reviews(id) ON DELETE CASCADE
 );
 
 -- migrate:down
